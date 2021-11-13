d731037e7157719b6e0a9b8f1aa25f11ea2f3806
trinhvanvinh <trinhvanvinh018@gmail.com>
2021-11-13 07:24:33 +0700

    save db

--- controllers/game.js
+++ controllers/game.js
@@ -3,16 +3,29 @@ var Hocvien = require("../models/Hocvien");
 
 module.exports = function (app){
     app.get("/", function (req, res){
+        res.send("OK");
+    })
 
-        var Teo = new Hocvien({
-            Email: "trinhvanTeo@gmail.com",
-            Hoten: "trinhvanTeo",
-            SoDT: "09090909",
-            ThanhToan: false,
-            Vi: "0x123",
-            Ngay: Date.now()
-        });
+    app.post("/dangky", function (req, res){
+        if(!req.body.Email || !req.body.Hoten || req.body.SoDT ){
+            res.json({ketqua: 0, maloi: "Thieu tham so"});
+        }else{
+            var hocvienMoi = new Hocvien({
+                Email: req.body.Email,
+                Hoten: req.body.Hoten,
+                SoDT: req.body.SoDT,
+                ThanhToan: false,
+                Vi: "",
+                Ngay: Date.now()
+            });
 
-        res.json(Teo);
+           hocvienMoi.save(function (err){
+               if(err){
+                   res.json({ketqua:0, maloi:"Mongodb save error"});
+               }else{
+                   res.json({ketqua:0, maloi:hocvienMoi});
+               }
+           })
+        }
     })
 }
